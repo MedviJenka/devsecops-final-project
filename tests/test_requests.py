df3110ada44fea88e2cb316cf78921484ec184ba
Jenia Petrusenko <evgeny.petrusenko@audiocodes.com>
2024-12-10 11:35:49 +0200

    dockerfile and compose update

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,14 +1,16 @@
 from app.roast_request import RoastRequest
 
 
+request = RoastRequest()
+
+
 def test() -> None:
-    request = RoastRequest()
     outcome = request.send_roast_request('hello')
+    print(outcome)
     assert '' in outcome
 
 
 def test_ai_server_health() -> None:
-    request = RoastRequest()
     outcome = request.health_check()
     assert outcome == 200
 
