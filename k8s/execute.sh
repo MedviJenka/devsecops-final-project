5cb38bb90639b951126c4bd7668837d0e491a6d4
jenia petrusenko <medvijenia@icloud.com>
2025-01-09 23:30:38 +0200

    bash

--- k8s/execute.sh
+++ k8s/execute.sh
@@ -1,5 +1,17 @@
-sudo
+#!/bin/bash
 minikube start
-echo "$(minikube ip) ai-bot.local.com" >> ~/etc/hosts
+minikube enable addons ingress
+
+MINIKUBE_IP=$(minikube ip)
+ENTRY="$MINIKUBE_IP ai-bot.local.com"
+
+if ! grep -qxF "$ENTRY" /etc/hosts; then
+  echo "$ENTRY" | sudo tee -a /etc/hosts
+  echo "Entry added: $ENTRY"
+else
+  echo "Entry already exists: $ENTRY"
+fi
+grep -qxF "$(minikube ip) ai-bot.local.com" /etc/hosts || echo "$(minikube ip) ai-bot.local.com" | sudo tee -a /etc/hosts
+
 kubectl apply -f .
 curl http://ai-bot.local.com/health
