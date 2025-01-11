9fd86daeedb4b1f541554e5f45b677bfb583f00f
jenia petrusenko <medvijenia@icloud.com>
2025-01-11 14:58:22 +0200

    ongres update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 minikube start
 minikube addons enable ingress
 kubectl create secret generic openai-api-key --from-literal=OPENAI_API_KEY=""
 
 MINIKUBE_IP=$(minikube ip)
-ENTRY="$MINIKUBE_IP ai-bot.local.com"
+ENTRY="$MINIKUBE_IP bot.k8s.com"
 
 if ! grep -qxF "$ENTRY" /etc/hosts; then
   echo "$ENTRY" | sudo tee -a /etc/hosts
@@ -12,7 +13,9 @@ if ! grep -qxF "$ENTRY" /etc/hosts; then
 else
   echo "Entry already exists: $ENTRY"
 fi
-grep -qxF "$(minikube ip) ai-bot.local.com" /etc/hosts || echo "$(minikube ip) ai-bot.local.com" | sudo tee -a /etc/hosts
+
+grep -qxF "$(minikube ip) bot.k8s.com" /etc/hosts || echo "$(minikube ip) bot.k8s.com" | sudo tee -a /etc/hosts
 
 kubectl apply -f k8s/.
-curl http://ai-bot.local.com/health
+
+curl http://bot.k8s.com/health
