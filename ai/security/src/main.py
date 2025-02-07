6f50aa18a79647ef285e0353c1d28d9eff9499c2
Jenia Petrusenko <evgeny.petrusenko@audiocodes.com>
2025-02-07 12:08:02 +0200

    ai security scan

--- ai/security/src/main.py
+++ ai/security/src/main.py
@@ -1,12 +1,15 @@
+from dataclasses import dataclass
+from textwrap import dedent
+from ai.k8s.status import K8SClusterManager
 from ai.security.src.crew import SecurityBase
 
 
+@dataclass
 class RunTestCrew(SecurityBase):
 
-    def run(self, topic: str):
-        inputs = {'topic': topic}
-        self.crew().kickoff(inputs=inputs)
+    k8s_manager = K8SClusterManager()
 
-
-a = RunTestCrew()
-a.run('API_KEY=1231')
+    def run(self) -> str:
+        inputs = {'topic': dedent(f"""{self.k8s_manager.list_deployments()}""")}
+        output = self.crew().kickoff(inputs=inputs)
+        return output
